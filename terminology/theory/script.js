71fedaa2c088effbb3a21d8d335a027846d864f7
yonganee1 <yonganee@icloud.com>
2024-04-22 19:35:29 +0600

    completed mobile version

---
+++ terminology/theory/script.js
@@ -0,0 +1,5 @@
+function changeSection() {
+    const selectElement = document.getElementById('sections');
+    const selectedSection = selectElement.value;
+    document.location=selectedSection;
+  }
